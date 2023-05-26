02db1d9ba7b3a2f6e6e768b761c4cb55bdf85d67
Dmytro Popov <thedmitryp@ukr.net>
2023-05-26 23:05:58 +0100

    Fixed unnecessary nullability
    ...of the result of the `languages` method.

--- lib/src/language_tool.dart
+++ lib/src/language_tool.dart
@@ -93,7 +93,7 @@ class LanguageTool {
   }
 
   /// Get a list of supported languages.
-  Future<List<Language>?> languages() async {
+  Future<List<Language>> languages() async {
     final res = await http.get(
       Uri.https(_url, 'v2/languages'),
       headers: {'Accept': 'application/json'},
